76aad1ad30c9d4ed3766cc0cfe260eb4cc1c293c
code-programer <93539221+code-programer@users.noreply.github.com>
2022-10-23 19:29:28 +0200

    Update simplify.js

--- simplify.js
+++ simplify.js
@@ -1,4 +1,22 @@
 var _ = {}
+//-------//
+//ARRAYS-//
+//-------//
+_.arr = {};
+//toArr()
+_.arr.toArr = function(w){
+  return w.split(",");
+}
+//toStr()
+_.arr.toStr(...w){
+  return w.toStr();
+}
+
+
+
+
+
+//help
 _.help = function(w){
   if(w === undefined){
     console.log(_)
